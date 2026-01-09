ee8b351673764cd48a7af35b17bc08d994470fb8
12345-dd <mayurpatil98607@gmail.com>
2026-01-09 11:40:03 +0530

    changes

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require("cors")
 const app = express()
 
 app.use(cors({
-    origin: process.env.CLIENT_URL || "https://jobanalytic.netlify.app/",
+    origin: process.env.CLIENT_URL || "https://jobanalytic.netlify.app",
     credentials: true
 }));
 app.use(express.json())
