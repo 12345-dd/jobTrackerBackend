fa0267ec5fe9a61f6532f3e641cfd6e8287761a0
12345-dd <mayurpatil98607@gmail.com>
2026-04-14 10:35:49 +0530

    Add health check route

--- app.js
+++ app.js
@@ -23,6 +23,10 @@ app.use(cors({
 
 app.use(express.json())
 
+app.get('/', (req, res) => {
+  res.json({ message: 'Job Tracker API is running ✅' });
+});
+
 const userRoutes = require("./routes/userRoutes")
 const appplicationRoutes = require("./routes/applicationRoutes")
 const resumeRoutes = require("./routes/resumeRoutes")
